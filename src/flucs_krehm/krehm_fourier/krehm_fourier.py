0bd6f3abc57b4fdb799a69a5ad3740290ddc38c3
physycola <toby.adkins1@gmail.com>
2026-06-06 18:30:33 -0700

    Refactor of compute_nonlinear_terms

--- src/flucs_krehm/krehm_fourier/krehm_fourier.py
+++ src/flucs_krehm/krehm_fourier/krehm_fourier.py
@@ -349,15 +349,14 @@ class KREHMFourier(FourierSystem):
         # Do anything model-specific here, then call the parent's method
         super().begin_time_step()
 
-    def calculate_nonlinear_terms(self) -> None:
+    def compute_nonlinear_terms(self, fields: cp.ndarray) -> None:
         """
-        Calculates the nonlinear terms. This is the most computationaly
-        intensive part of taking a time step. Here, we also determine the
-        nonlinear CFL coefficient.
+        Computes the nonlinear terms for the supplied fields. Here, we also
+        determine the nonlinear CFL coefficient.
 
         """
         self.find_derivatives_kernel(
-            self.fields[self.current_step % 2 - 1],
+            fields,
             self.dft_derivatives,
             self.cfl_rate
         )
@@ -376,8 +375,6 @@ class KREHMFourier(FourierSystem):
 
         # NB: real_derivatives and real_bits are the same array
         self.plan_bits_r2c.fft(self.real_bits, self.dft_bits, cufft.CUFFT_FORWARD)
-
-        super().calculate_nonlinear_terms()
 
     def finish_time_step(self) -> None:
         super().finish_time_step()
