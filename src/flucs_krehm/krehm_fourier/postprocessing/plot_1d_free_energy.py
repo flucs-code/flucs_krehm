73d574bf7320adc5254f122d818b9daa59e9056d
Plamen Ivanov <plamen.ivanov257@gmail.com>
2026-06-26 15:45:57 +0100

    minor plotting changes

--- src/flucs_krehm/krehm_fourier/postprocessing/plot_1d_free_energy.py
+++ src/flucs_krehm/krehm_fourier/postprocessing/plot_1d_free_energy.py
@@ -23,7 +23,7 @@ def plot_1d_free_energy(post, args):
     # Initialise plotting
     fig_w, ax_w = plt.subplots(1, 1, layout="constrained")
 
-    ymin = 1e-6
+    ymin = 1e-12
     xscale = "log"
     yscale = "log"
 
